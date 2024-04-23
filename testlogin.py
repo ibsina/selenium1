80ff22c39ed1e2a264f0e7b02645e47e6114855a
ibsina <35305255+ibsina@users.noreply.github.com>
2024-04-23 23:15:13 +0700

    Update testlogin.py

--- testlogin.py
+++ testlogin.py
@@ -4,7 +4,7 @@ driver = webdriver.Firefox()
 url = "https://emr.fortiseahk.com"  
 for _ in range(20):      
   driver.get(url)
-  driver.find_element(By.ID,"authUser").send_keys("admin")
+  driver.find_element(By.ID,"authUser").send_keys("docter")
   driver.find_element(By.ID,"clearPass").send_keys("testa412315%")
   driver.find_element(By.ID,"login-button").click()
 driver.quit() 
