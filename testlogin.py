0722f81b20a6b7820cb88f24af04797717ebcd05
ibsina <35305255+ibsina@users.noreply.github.com>
2024-04-23 23:15:51 +0700

    Update testlogin.py

--- testlogin.py
+++ testlogin.py
@@ -1,10 +1,10 @@
 from selenium import webdriver 
 from selenium.webdriver.common.by import By
 driver = webdriver.Firefox()
-url = "https://emr.fortiseahk.com"  
+url = "https://<your url test"  
 for _ in range(20):      
   driver.get(url)
-  driver.find_element(By.ID,"authUser").send_keys("docter")
-  driver.find_element(By.ID,"clearPass").send_keys("testa412315%")
+  driver.find_element(By.ID,"authUser").send_keys("user")
+  driver.find_element(By.ID,"clearPass").send_keys("pass")
   driver.find_element(By.ID,"login-button").click()
 driver.quit() 
